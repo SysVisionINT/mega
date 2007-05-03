ff71f6d3b3edfbd2bf850c621df798efedbfd1f7
jrocha <jrocha@56d715eb-3f8e-4b33-f840-b1c5373715b9>
2007-05-03 10:53:15 +0000

    *** empty log message ***

---
+++ trunk/SRC/net/java/mega/tags/form/RadioButtonTag.java
@@ -0,0 +1,68 @@
+/*  ------------------
+ *  MEGA Web Framework
+ *  ------------------
+ *  
+ *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package net.java.mega.tags.form;
+
+import javax.servlet.jsp.JspException;
+
+import net.java.mega.tags.model.InputBaseTag;
+import net.java.sjtools.logging.Log;
+import net.java.sjtools.logging.LogFactory;
+
+public class RadioButtonTag extends InputBaseTag {
+	private static final long serialVersionUID = -1717073186853398272L;
+
+	private static Log log = LogFactory.getLog(RadioButtonTag.class);
+	
+	private String value = null;
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public int writeStartTag() throws JspException {		
+		return NOT_INCLUDE_INNER_HTML;
+	}
+	
+	public void writeEndTag() throws JspException {
+		try {
+			pageContext.getOut().print("<input type=\"radio\" name=\"");
+			pageContext.getOut().print(getProperty());
+			pageContext.getOut().print("\" value=\"");
+			pageContext.getOut().print(getValue());
+			pageContext.getOut().print("\"");
+			writeAttributes();
+			
+			if (getValue().equals(String.valueOf(getPropertyValue()))) {
+				pageContext.getOut().print(" checked=\"checked\"");
+			}
+			
+			pageContext.getOut().println("/>");
+		} catch (Exception e) {
+			log.error("Error while writing radio TAG", e);
+			throw new JspException(e);
+		}
+	}
+
+	public void initTag() {		
+	}
+}
