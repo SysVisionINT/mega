4fbf45e5548d1abf1923a445c059f0dfb3121fde
goncalo_pereira <goncalo_pereira@56d715eb-3f8e-4b33-f840-b1c5373715b9>
2008-02-08 17:25:38 +0000

    Issue number: Obtained from: Submitted by:  Gon�o Pereira Reviewed by:

--- trunk/SRC/net/java/mega/tags/form/SubmitTag.java
+++ trunk/SRC/net/java/mega/tags/form/SubmitTag.java
@@ -1,3 +1,21 @@
+/*  ------------------
+ *  MEGA Web Framework
+ *  ------------------
+ *  
+ *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package net.java.mega.tags.form;
 
 import javax.servlet.jsp.JspException;
@@ -8,11 +26,10 @@ import net.java.sjtools.logging.LogFactory;
 
 public class SubmitTag extends SubmitBaseTag {
 	private static final long serialVersionUID = 6684109710319221195L;
-	
+
 	private static Log log = LogFactory.getLog(SubmitTag.class);
-	
-	public void initTag() {
-	}
+
+	public void initTag() {}
 
 	public int writeStartTag() throws JspException {
 		return NOT_INCLUDE_INNER_HTML;
@@ -20,7 +37,7 @@ public class SubmitTag extends SubmitBaseTag {
 
 	public void writeEndTag() throws JspException {
 		String value = getInnerHtml();
-		
+
 		try {
 			pageContext.getOut().print("<input type=\"button\" value=\"");
 			pageContext.getOut().print(value);
