f828073f34dc145510ea4b25a4b498b4a0a2f5a3
Gonçalo Pereira <goncalo-pereira@users.noreply.github.com>
2008-02-08 17:25:38 +0000

    Issue number: Obtained from: Submitted by:  Gon�o Pereira Reviewed by:

--- SRC/net/java/mega/tags/form/LinkSubmitTag.java
+++ SRC/net/java/mega/tags/form/LinkSubmitTag.java
@@ -1,3 +1,21 @@
+/*  ------------------
+ *  MEGA Web Framework
+ *  ------------------
+ *  
+ *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *  	http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package net.java.mega.tags.form;
 
 import javax.servlet.jsp.JspException;
@@ -8,11 +26,10 @@ import net.java.sjtools.logging.LogFactory;
 
 public class LinkSubmitTag extends SubmitBaseTag {
 	private static final long serialVersionUID = 6684109710319221195L;
-	
+
 	private static Log log = LogFactory.getLog(LinkSubmitTag.class);
-	
-	public void initTag() {
-	}
+
+	public void initTag() {}
 
 	public int writeStartTag() throws JspException {
 		try {
@@ -25,11 +42,11 @@ public class LinkSubmitTag extends SubmitBaseTag {
 			log.error("Error while writing INPUT a (submit) TAG", e);
 			throw new JspException(e);
 		}
-		
+
 		return INCLUDE_INNER_HTML;
 	}
 
-	public void writeEndTag() throws JspException {		
+	public void writeEndTag() throws JspException {
 		try {
 			pageContext.getOut().println("</a>");
 		} catch (Exception e) {
