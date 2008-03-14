a4dbf287814b49f57e28cbd8869a9a5286d88fe6
jrocha <jrocha@56d715eb-3f8e-4b33-f840-b1c5373715b9>
2008-03-14 12:19:35 +0000

    *** empty log message ***

--- trunk/SRC/net/java/mega/action/util/FormatUtil.java
+++ trunk/SRC/net/java/mega/action/util/FormatUtil.java
@@ -1,15 +1,15 @@
 /*  ------------------
  *  MEGA Web Framework
  *  ------------------
- *  
+ *
  *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *  	http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,7 @@ import net.java.sjtools.logging.Log;
 import net.java.sjtools.logging.LogFactory;
 
 public class FormatUtil {
+
 	private static Log log = LogFactory.getLog(FormatUtil.class);
 
 	public static String format(Object obj, Locale locale) {
@@ -40,6 +41,10 @@ public class FormatUtil {
 	}
 
 	public static String format(Object obj, String format, Locale locale) {
+		return format(obj, format, locale, true);
+	}
+
+	public static String format(Object obj, String format, Locale locale, boolean filter) {
 		if (log.isDebugEnabled()) {
 			log.debug("format(" + obj + "," + format + ")");
 		}
@@ -48,60 +53,70 @@ public class FormatUtil {
 			return "";
 		}
 
-		Format formater = null;
+		String retString = null;
 
 		if (obj instanceof String) {
-			return HTMLUtil.filter((String) obj);
-		} else if (obj instanceof Number) {
-			String formatString = format;
+			retString = (String) obj;
+		} else {
+			Format formater = null;
 
-			if (format == null) {
-				formatString = retrieveFormatString(obj.getClass(), locale);
-				
-				if (log.isDebugEnabled()) {
-					log.debug(obj.getClass().getName() + "  -> formatString = " + formatString);
+			if (obj instanceof Number) {
+				String formatString = format;
+
+				if (format == null) {
+					formatString = retrieveFormatString(obj.getClass(), locale);
+
+					if (log.isDebugEnabled()) {
+						log.debug(obj.getClass().getName() + "  -> formatString = " + formatString);
+					}
+				}
+
+				if (formatString != null) {
+					formater = NumberFormat.getNumberInstance();
+
+					((DecimalFormat) formater).applyLocalizedPattern(formatString);
+				}
+			} else if (obj instanceof java.util.Date) {
+				String formatString = format;
+
+				if (format == null) {
+					formatString = retrieveFormatString(obj.getClass(), locale);
+
+					if (log.isDebugEnabled()) {
+						log.debug(obj.getClass().getName() + "  -> formatString = " + formatString);
+					}
+				}
+
+				if (formatString != null) {
+					formater = new SimpleDateFormat(formatString);
 				}
 			}
 
-			if (formatString != null) {
-				formater = NumberFormat.getNumberInstance();
-
-				((DecimalFormat) formater).applyLocalizedPattern(formatString);
-			}
-		} else if (obj instanceof java.util.Date) {
-			String formatString = format;
-
-			if (format == null) {
-				formatString = retrieveFormatString(obj.getClass(), locale);
-				
-				if (log.isDebugEnabled()) {
-					log.debug(obj.getClass().getName() + "  -> formatString = " + formatString);
-				}
-			}
-
-			if (formatString != null) {
-				formater = new SimpleDateFormat(formatString);
+			if (formater != null) {
+				retString = formater.format(obj);
+			} else {
+				retString = obj.toString();
 			}
 		}
 
-		if (formater != null) {
-			return formater.format(obj);
+		if (filter) {
+			return HTMLUtil.filter(retString);
 		} else {
-			return obj.toString();
+			return retString;
 		}
 	}
 
 	private static String retrieveFormatString(Class clazz, Locale locale) {
 		String format = null;
-		
+
 		Class objectClass = clazz;
-		
-		do {		
+
+		do {
 			format = ActionMessageUtil.getMessage(objectClass.getName(), locale);
 			objectClass = objectClass.getSuperclass();
 		} while (format == null && objectClass != null);
-		
-		
+
+
 		return format;
 	}
 }
--- trunk/SRC/net/java/mega/layout/tag/MessageKeyBlockTag.java
+++ trunk/SRC/net/java/mega/layout/tag/MessageKeyBlockTag.java
@@ -1,15 +1,15 @@
 /*  ------------------
  *  MEGA Web Framework
  *  ------------------
- *  
+ *
  *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *  	http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@ import net.java.mega.layout.model.MessageKeyContent;
 
 public class MessageKeyBlockTag extends BaseBlockTag {
 	private static final long serialVersionUID = -427852391259046306L;
-	
+
 	private String value = null;
-	private String filter = "true";
+	private boolean filter = true;
 
 	public String getValue() {
 		return value;
@@ -34,21 +34,20 @@ public class MessageKeyBlockTag extends BaseBlockTag {
 	public void setValue(String value) {
 		this.value = value;
 	}
-	
-	
-	public String getFilter() {
+
+	public boolean isFilter() {
 		return filter;
 	}
 
-	public void setFilter(String filter) {
+	public void setFilter(boolean filter) {
 		this.filter = filter;
 	}
 
 	public BlockContent getBlockContent() {
 		MessageKeyContent content = new MessageKeyContent();
-		
+
 		content.setValue(getValue());
-		content.setFilter(getFilter().equals("true"));
+		content.setFilter(filter);
 
 		return content;
 	}
