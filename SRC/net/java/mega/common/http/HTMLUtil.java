988eec6bc33cbdf86621937de9705f8703782707
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2008-07-08 15:22:11 +0000

    *** empty log message ***

--- SRC/net/java/mega/common/http/HTMLUtil.java
+++ SRC/net/java/mega/common/http/HTMLUtil.java
@@ -1,15 +1,15 @@
 /*  ------------------
  *  MEGA Web Framework
  *  ------------------
- *  
+ *
  *  Copyright 2006 SysVision - Consultadoria e Desenvolvimento em Sistemas de Informática, Lda.
- *  
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- *  
+ *
  *  	http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,10 @@
 package net.java.mega.common.http;
 
 public class HTMLUtil {
-	private static final String CHAR_LIST = "«»?<>&\"'ºªçÇñÑáàãâÁÀÃÂéèêÉÈÊíìîÍÌÎóòõôÓÒÕÔúùûÚÙÛ";
-	private static final String[] ENT_LIST = { "&laquo;", "&raquo;", "&euro;", "&lt;", "&gt;", "&amp;", "&quot;",
-			"&#39;", "&ordm;", "&ordf;", "&ccedil;", "&Ccedil;", "&ntilde;", "&Ntilde;", "&aacute;", "&agrave;",
-			"&atilde;", "&acirc;", "&Aacute;", "&Agrave;", "&Atilde;", "&Acirc;", "&eacute;", "&egrave;", "&ecirc;",
-			"&Eacute;", "&Egrave;", "&Ecirc;", "&iacute;", "&igrave;", "&icirc;", "&Iacute;", "&Igrave;", "&Icirc;",
-			"&oacute;", "&ograve;", "&otilde;", "&ocirc;", "&Oacute;", "&Ograve;", "&Otilde;", "&Ocirc;", "&uacute;",
-			"&ugrave;", "&ucirc;", "&Uacute;", "&Ugrave;", "&Ucirc;" };
+
+	private static final String CHAR_LIST = "«»<>&\"'ºªçÇñÑáàãâÁÀÃÂéèêÉÈÊíìîÍÌÎóòõôÓÒÕÔúùûÚÙÛ";
+	private static final String[] ENT_LIST = { "&laquo;", "&raquo;", "&lt;", "&gt;", "&amp;", "&quot;", "&#39;", "&ordm;", "&ordf;", "&ccedil;", "&Ccedil;", "&ntilde;", "&Ntilde;", "&aacute;", "&agrave;", "&atilde;", "&acirc;", "&Aacute;", "&Agrave;", "&Atilde;", "&Acirc;", "&eacute;", "&egrave;", "&ecirc;", "&Eacute;", "&Egrave;", "&Ecirc;", "&iacute;", "&igrave;", "&icirc;", "&Iacute;", "&Igrave;", "&Icirc;", "&oacute;", "&ograve;", "&otilde;", "&ocirc;", "&Oacute;", "&Ograve;", "&Otilde;",
+			"&Ocirc;", "&uacute;", "&ugrave;", "&ucirc;", "&Uacute;", "&Ugrave;", "&Ucirc;" };
 
 	public static String filter(String obj) {
 		if (obj == null) {
